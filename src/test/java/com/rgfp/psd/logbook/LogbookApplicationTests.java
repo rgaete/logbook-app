c4e8d9c641b4e1c5e2b86d934a0261418e7ff40c
Ricardo Gaete Duarte <ricardo.gaete@latam.com>
2018-06-10 01:13:50 -0400

    Adding Context Test Run

--- src/test/java/com/rgfp/psd/logbook/LogbookApplicationTests.java
+++ src/test/java/com/rgfp/psd/logbook/LogbookApplicationTests.java
@@ -1,16 +1,24 @@
 package com.rgfp.psd.logbook;
 
+import com.rgfp.psd.logbook.service.NoteService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LogbookApplicationTests {
 
+	@Autowired
+	private NoteService noteService;
+
 	@Test
 	public void contextLoads() {
+		assertNotNull(noteService);
 	}
 
 }
